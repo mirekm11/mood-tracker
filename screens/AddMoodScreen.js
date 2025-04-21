cbe567fc564dcc8292c3d3adfccef90043c01fb8
Miroslaw Makara <mirek.makara@gmail.com>
2025-04-21 16:40:40 +0100

    Utworzenie projektu, struktura folderów, podstawowa nawigacja

---
+++ screens/AddMoodScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function AddMoodScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Add Mood Screen</Text>
+    </View>
+  );
+}
---
+++ screens/MoodDetailsScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function MoodDetailsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Mood Details Screen</Text>
+    </View>
+  );
+}
